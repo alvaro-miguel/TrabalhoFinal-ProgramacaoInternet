d07e108e08109e81737420b3377b80eefe4d0350
alvaro-miguel <alvaromiguelrodrigues12@gmail.com>
2026-06-28 20:05:38 -0300

    Correcao do caminho estatico do frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const alunoRoutes = require('./routes/alunoRoutes');
 const { initDatabase } = require('./database');
 
@@ -8,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
-
+app.use(express.static(path.join(__dirname, '../frontend')));
+ 
 initDatabase();
 
 app.use('/api', alunoRoutes);
